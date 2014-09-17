d2795c7c9fb33b64849bc0c85a11d19fb79f2408
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-09-16 20:19:45 -0700

    Fixed data colection for Moreover tags

--- modules/MoreoverStoryFilter.js
+++ modules/MoreoverStoryFilter.js
@@ -18,11 +18,54 @@ var MoreoverStoryFilter = {
 
   getTopics: function(doc, obj) {
     if (doc.topics && doc.topics instanceof Object && doc.topics.topic instanceof Array) {
-      obj.topics = {groups: {}, names: {}};
+      var topics = {};
       doc.topics.topic.forEach(function(item) {
-        obj.topics.groups[item.group] = true;
-        obj.topics.names[item.name] = true;
+        topics[item.group] = true;
+        var name = item.name.replace(/ news$/,"");
+        topics[name] = true;
       });
+      var keys = Object.keys(topics);
+      if (keys.length > 0) {
+        obj.topics = keys;
+      }
+    }
+  },
+
+  getCompanies: function(doc, obj) {
+    if (doc.topics && doc.companies instanceof Object) {
+      if (doc.companies.company instanceof Array) {
+        var comps = {};
+        doc.companies.company.forEach(function(comp) {
+          comps[comp.name] = true;
+        });
+        obj.companies = Object.keys(comps);
+      } else if (doc.companies.company instanceof Object) {
+        obj.companies = [doc.companies.company.name];
+      }
+    }
+  },
+
+  getSemantic: function(doc, obj) {
+    var props = {};
+    function objectWalker(object, parentKey) {
+      if (object instanceof Array) {
+        object.forEach(function(child) {
+          objectWalker(child, parentKey);
+        });
+      }
+      else if (parentKey == "property" && object.name == "value") {
+        props[object.value] = true;
+      }
+      else if (object instanceof Object) {
+        Object.keys(object).forEach(function(key) {
+          objectWalker(object[key], key);
+        });
+      }
+    };
+    objectWalker(doc.semantics, "semantics");
+    var keys = Object.keys(props);
+    if (keys.length > 0) {
+      obj.semantics = keys;
     }
   },
 
@@ -49,6 +92,8 @@ var MoreoverStoryFilter = {
     this.getTags(doc, obj);
     this.getImage(doc, obj);
     this.getTopics(doc, obj);
+    this.getCompanies(doc, obj);
+    this.getSemantic(doc, obj);
     return obj;
   },
 };
---
+++ test/modules/testMoreoverFilter.js
@@ -0,0 +1,20 @@
+'use strict';
+var should = require('should');
+var xml2js = require("xml2js");
+var moreover = require("../data/MoreoverStoryFilter");
+var moreoverFilter = require("../../modules/MoreoverStoryFilter");
+
+
+describe('test host saver', function(){
+  it('parse and filter', function(done) {
+    var xmlBody = moreover.moreoverXmlExample;
+    xml2js.parseString(xmlBody, {explicitArray: false}, function (err, result) {
+      var doc = result.articles.article[0];
+      var filtered = moreoverFilter.filter(doc);
+      //console.log(JSON.stringify(filtered, null, 1));
+      done();
+    });
+  });
+
+});
+
