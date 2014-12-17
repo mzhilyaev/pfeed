0d2b19a83505f48e73435bdb7e77e8df4a3768a4
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2014-12-17 15:49:05 -0800

    Clarifing comments and variable name changes

--- stats/StatsCollector.js
+++ stats/StatsCollector.js
@@ -6,50 +6,51 @@ function StatsCollector() {
 
 StatsCollector.prototype = {
   // adds results of classification to catStats
-  add: function(domain, ruleSet, expected, assigned) {
+  add: function(domain, ruleSet, expected, categorized) {
     if (!this.catStats["ANY"]) this.catStats["ANY"] = {};
-    this.addToStatsObj(this.catStats["ANY"], ruleSet, expected, assigned);
+    this.addToStatsObj(this.catStats["ANY"], ruleSet, expected, categorized);
     // skip domain statistics for now
     //if (!this.catStats[domain]) this.catStats[domain] = {};
-    //addToStatsObj(this.catStats[domain], ruleSet, expected, assigned);
+    //addToStatsObj(this.catStats[domain], ruleSet, expected, categorized);
   },
 
   // adds results of classification to a single stats object
-  addToStatsObj: function(stats, ruleSet, expected, assigned) {
+  addToStatsObj: function(stats, ruleSet, expected, categorized) {
     if (!stats[ruleSet]) {
       stats[ruleSet] = {};
     }
     var statsObj = stats[ruleSet];   // stats object
     var eCats = {};  // expected cats map
-    var aCats = {};  // assigned cats map
+    var aCats = {};  // categorized cats map
 
     // makes an empty object of cat statistics
     function makeCatStats() {
       return {
-        seen: 0,     // expected total count
-        assigned: 0, // assigned total count
-        correct: 0,  // assigned correct count
+        total: 0,     // expected total count
+        categorized: 0, // categorized total count
+        correct: 0,  // categorized correct count
       };
     }
 
-    // populate expected cats maps and update seen count
+    // populate expected cats maps and update total count
     expected.forEach(function(cat) {
       eCats[cat] = true;
       if (!statsObj[cat]) statsObj[cat] = makeCatStats();
-      statsObj[cat].seen++;
+      statsObj[cat].total++;
     });
 
-    // process assigned categories
-    assigned.forEach(function(cat) {
+    // process categorized categories
+    categorized.forEach(function(cat) {
       if (aCats[cat]) return;  // do not allow duplicates
       if (!statsObj[cat]) statsObj[cat] = makeCatStats();
 
-      statsObj[cat].assigned++;  // update total assinged count
+      statsObj[cat].categorized++;  // update total assinged count
       if (eCats[cat]) statsObj[cat].correct++; // update correct count
       aCats[cat] = true; // mark category as processed
     });
   },
 
+  // output function
   output: function() {
     var domains = Object.keys(this.catStats);
     for (var i in domains) {
@@ -60,8 +61,8 @@ StatsCollector.prototype = {
         var cats = Object.keys(ruleSetsObject[ruleSet]).sort();
         cats.forEach(function(cat) {
           var catStats = ruleSetsObject[ruleSet][cat];
-          var precision = (catStats.assigned) ? catStats.correct / catStats.assigned : "?";
-          var recall = (catStats.seen) ? catStats.correct / catStats.seen : 0;
+          var precision = (catStats.categorized) ? catStats.correct / catStats.categorized : "?";
+          var recall = (catStats.total) ? catStats.correct / catStats.total : 0;
           console.log("  ", cat, precision, recall);
         });
       });
