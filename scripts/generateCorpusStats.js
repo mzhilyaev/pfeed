886630afe6f4cbdc4061571e7d22ffbbc7b2c4e1
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2015-01-08 17:04:13 -0800

    Closes Bug 1119606 - [back-end] Implement script to generate Url/Title rules for each document

--- scripts/generateCorpusStats.js
+++ scripts/generateCorpusStats.js
@@ -26,7 +26,9 @@ function addToStats(key, cats) {
   });
 };
 
-function accumulate(url, title, topics) {
+function accumulate(url, title, topics, urlHash) {
+  url = url.toLowerCase();
+  title = title.toLowerCase();
   // compute IAB cats
   var cats = {};
   topics.forEach(function(topic) {
@@ -39,33 +41,99 @@ function accumulate(url, title, topics) {
   });
 
   var catNames = Object.keys(cats);
+  var terms = {};
 
-  function outputTerms(text, suffix) {
-    var terms = {};
+  function addToTerms(data, suffix, doBigrams) {
     var lastTerm;
-    text.toLowerCase().replace(kNotWordPattern, " ").split(/\s+/).forEach(function(term) {
+    var tokens = data;  // assume data is array of tokens
+    if (typeof(data) == "string") {
+      // data is string, hence tokenize it
+      tokens = data.toLowerCase().replace(kNotWordPattern, " ").split(/\s+/);
+    }
+    // walk tokens and fill in terms object
+    tokens.forEach(function(term) {
       if (term.match(/[a-z]/) && !stopWords[term] && term.length > 2 && term.length < 20) {
         terms[term+suffix] = true;
-        if (lastTerm) {
-          terms[lastTerm+term+suffix] = true;
+        if (lastTerm && doBigrams) {
+          terms[lastTerm + "+" + term + suffix] = true;
         }
         lastTerm = term;
       }
     });
+  }
 
-    //console.log(url, title, JSON.stringify(Object.keys(terms)));
+  function unconditionalAddToTerms(tokens, suffix) {
+    tokens.forEach(function(term) {
+      terms[term+suffix] = true;
+    });
+  }
+
+  function processUrl(url) {
+    var urlObj = require("url").parse(url);
+    var host = urlObj.hostname;
+    var domain = require('tldjs').getDomain(host);
+
+    // do host first
+    host = host.replace(/^www\./,"");  // no www
+    host = host.replace(/^rss\./,"");  // no rss
+    host = host.substring(0, host.length - domain.length); // no domain string
+    // explicitly tokenize host string on period, keep all other punctuation
+    // add tokens on period bounaries, no bigrams
+    var hostChunks = host.split(".");
+    addToTerms(hostChunks, "_U", false);
+
+    // add domain
+    unconditionalAddToTerms([domain], "_D", false);
+
+    // add host rules
+    var hostStr = domain;
+    hostChunks.reverse().forEach(function(chunk) {
+      if (chunk && chunk != "") {
+        hostStr = chunk + "." + hostStr;
+        unconditionalAddToTerms([hostStr], "_H", false);
+      }
+    });
+
+    // deal with paths
+    var pathChunks = urlObj.pathname.split("/");
+    // remove chunks that are too long (> 30), or have too many [-_] in them
+    for (var i in pathChunks) {
+      var chunk = pathChunks[i];
+      if (chunk == "" ||
+          chunk.length > 30 ||
+          chunk.split(/[-_]/).length > 3) {
+          pathChunks[i] = null;
+      }
+      else {
+        // it's an OK chunk, so generate _U and _P terms
+        unconditionalAddToTerms([domain + "/" + chunk], "_P", false);
+        addToTerms(chunk, "_U", true);
+      }
+    }
+  }
+
+  function outputTerms() {
     Object.keys(terms).forEach(function(term) {
-      console.log(term);
+      console.log(term + "," + urlHash);
       catNames.forEach(function(cat) {
-        console.log(term + ":" + cat);
+        console.log(term + ":" + cat + "," + urlHash);
       });
     });
   }
 
+  function outputCats() {
+    catNames.forEach(function(term) {
+      console.log(term + "_C," + urlHash);
+    });
+  }
+
+  // if categorization is assgined, process terms
   if (catNames.length > 0) {
-    outputTerms(url, "_U");
-    outputTerms(title, "_T");
-    outputTerms(url + " " + title, "_A");
+    processUrl(url);
+    addToTerms(title, "_T", true);
+    outputTerms();
+    outputCats();
+    // store assigned classification
     addToStats("_ANY", catNames);
   }
 };
