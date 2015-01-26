eabd06b75c8cd9ae91a1942ba8ea707abed8da80
Maxim Zhilyaev <mzhilyaev@mozilla.com>
2015-01-26 12:25:34 -0800

    Move HostDocReader into a separate module

--- modules/HostSaver.js
+++ modules/HostSaver.js
@@ -6,55 +6,12 @@ var zlib = require("zlib");
 
 var config = require("../config/config");
 var utils = require("./Utils");
+var HostDocReader = require("./HostDocReader");
 
 // descending order compare function
 function descOrderComp(a, b) {
   return (a<b) ? 1 : ((b==a) ? 0 : -1);
 }
-
-function HostDocReader(hostDir, startFile) {
-  this.hostDir = hostDir;
-  // sort files in descending order - youngest file first
-  this.files = fs.readdirSync(hostDir).sort(descOrderComp);
-  if (startFile) {
-    while (this.files.length && this.files[0] <= startFile) {
-      this.files.shift();
-    }
-  }
-};
-
-HostDocReader.prototype = {
-  next: function(cb) {
-    if (this.files.length == 0) {
-      // execute callback when function exists
-      setTimeout(function() {cb(null);}, 0);
-      return;
-    }
-    // otherwise pick the next file and process it
-    var file = this.files.shift();
-    var docArray = [];
-    var lineStream = new LineStream();
-    lineStream.on('data', function(line) {
-      try {
-        var doc = JSON.parse(line);
-        docArray.push(doc);
-      } catch (e) {}
-    });
-    lineStream.on('end', function() {
-      // execute callback when function exists
-      setTimeout(function() {cb(docArray, file);}, 0);
-    });
-    var fileStream = fs.createReadStream(path.join(this.hostDir, file));
-    // @TODO - rew-write with endsWith
-    if (file.match(/.gz$/)) {
-      var gunzipStream = zlib.createGunzip();
-      fileStream.pipe(gunzipStream).pipe(lineStream);
-    }
-    else {
-      fileStream.pipe(lineStream);
-    }
-  },
-};
 
 HostSaver = {
   init: function() {
